f9b2b228cbd71bf7df6d85724eb29f0a562c04cf
Keizo410 <keizo.kato410@gmail.com>
2024-10-03 15:45:40 -0700

    update

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from extractor.collect import JobDescriptionCollector
 
 
 def main():
-    # url = "https://www.glassdoor.ca/Job/data-engineer-jobs-SRCH_KO0,13.htm"
-    # url="https://www.glassdoor.ca/Job/data-analyst-jobs-SRCH_KO0,12.htm"
-    # url = "https://www.glassdoor.ca/Job/vancouver-entry-level-software-developer-jobs-SRCH_IL.0,9_IC2278756_KO10,40.htm"
-    url = "https://www.glassdoor.ca/Job/vancouver-bc-data-analyst-jobs-SRCH_IL.0,12_IC2278756_KO13,25.htm"
+   
+    url = "https://www.glassdoor.ca/Job/vancouver-c-developer-jobs-SRCH_IL.0,9_IC2278756_KO10,21.htm"
     output_csv = 'job_data'
 
     collector = JobDescriptionCollector(num_jobs=3, file_name = output_csv)
